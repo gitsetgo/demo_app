801d12c786e04e7d6ea5a659aecfb4f367723b69
Gitter Done <gitsetdone@gmail.com>
2011-02-06 12:52:34 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8b13ba629171f48644fef0aacd32ae20d6c926d40166c20881b8177bbcee43b22970cc797ea402fceee6e67ff4f85aa3fe85aa06b53d5806a2a229602c781cdb'
